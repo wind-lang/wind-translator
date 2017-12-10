4e92a8979c8f91009a94069b6107ed770a092508
Joshua Weinstein <jweinstein@splunk.com>
2017-12-09 23:44:30 -0800

    started wind instruction

--- include/Instruction.h
+++ include/Instruction.h
@@ -1,4 +1,11 @@
 #ifndef WIND_INSTRUCTION_H
 #define WIND_INSTRUCTION_H
 
+typedef enum
+{
+        WindInstruc_Int,
+        WindInstruc_ExpStart,
+        WindInstruc_ExpEnd
+} WindInstruc;
+
 #endif
